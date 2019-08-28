4f383bcfca925e903d446a652858d3141d0972d9
pyj <631967081@qq.com>
2019-08-28 17:21:18 +0800

    8.28

---
+++ src/dynamicprogramming/LeetCode44.java
@@ -0,0 +1,45 @@
+package dynamicprogramming;
+
+/*
+给定一个字符串 (s) 和一个字符模式 (p) ，实现一个支持 '?' 和 '*' 的通配符匹配。
+'?' 可以匹配任何单个字符。
+'*' 可以匹配任意字符串（包括空字符串）。
+两个字符串完全匹配才算匹配成功。
+
+说明:
+s 可能为空，且只包含从 a-z 的小写字母。
+p 可能为空，且只包含从 a-z 的小写字母，以及字符 ? 和 *。
+ */
+
+/*
+https://leetcode-cn.com/problems/wildcard-matching/solution/shuang-zhi-zhen-he-dong-tai-gui-hua-by-powcai/
+动态规划方程：
+1.如果(s[i] == p[j] || p[j] == "?") && dp[i-1][j-1] ,有dp[i][j] = true
+2.如果p[j] == "*" && (dp[i-1][j] = true || dp[i][j-1] = true)有dp[i][j] = true
+dp[i][j-1],表示*代表是空字符,例如ab,ab*
+dp[i-1][j],表示*代表非空任何字符,例如abcd,ab*
+ */
+public class LeetCode44 {
+    public boolean isMatch(String s, String p) {
+        boolean dp[][] = new boolean[s.length() + 1][p.length() + 1];
+        dp[0][0] = true;
+        for (int j = 1; j < p.length() + 1; j++) {
+            // p[j] == '*' dp[0][j] = dp[0][j - 1]
+            if (p.charAt(j - 1) == '*') {
+                dp[0][j] = dp[0][j - 1];
+            }
+        }
+        for (int i = 1; i < s.length() + 1; i++) {
+            for (int j = 1; j < p.length() + 1; j++) {
+                // s[i] == p[j] || p[j] == "?"
+                if (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '?') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (p.charAt(j - 1) == '*') {
+                    //p[j] == "*" && (dp[i-1][j] = true || dp[i][j-1] = true)
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                }
+            }
+        }
+        return dp[s.length()][p.length()];
+    }
+}
